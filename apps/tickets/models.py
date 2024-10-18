74af3d1dd0f63109eb645fb394588ca240cf7cec
xts-sec <30200315+xts-sec@users.noreply.github.com>
2024-10-18 07:48:34 +0100

    Made priority selectable from 3 choices in the model, and a drop down in forms. Fixed status displaying the backend value and not the readable name. Fixed formatting for some pages so all visually similar.

--- apps/tickets/models.py
+++ apps/tickets/models.py
@@ -2,26 +2,43 @@ from django.db import models
 
 # Create your models here.
 
+
 class Project(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
 
+
 class Issue(models.Model):
     STATUS_CHOICES = [
-        ('TODO', 'TO DO'),
-        ('IN_PROGRESS', 'IN PROGRESS'),
-        ('DONE', 'DONE'),
+        ("TODO", "To Do"),
+        ("IN_PROGRESS", "In Progress"),
+        ("DONE", "Done"),
+    ]
+
+    PRIORITY_CHOICES = [
+        ("LOW", "Low"),
+        ("MEDIUM", "Medium"),
+        ("HIGH", "High"),
     ]
 
     title = models.CharField(max_length=200)
     description = models.TextField()
-    priority = models.CharField(max_length=50)
+
+    priority = models.CharField(
+        max_length=12,
+        choices=PRIORITY_CHOICES,
+        default="Medium",
+        null=False,
+        blank=False,  # Ensure that blank or null values are not allowed
+    )
+
+
     status = models.CharField(
-        max_length=12, 
-        choices=STATUS_CHOICES, 
-        default='TODO', 
-        null=False, 
-        blank=False  # Ensure that blank or null values are not allowed
+        max_length=12,
+        choices=STATUS_CHOICES,
+        default="TODO",
+        null=False,
+        blank=False,  # Ensure that blank or null values are not allowed
     )
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -29,6 +46,7 @@ class Issue(models.Model):
 
     def __str__(self):
         return self.title
+
 
 class Backlog(models.Model):
     issue = models.ForeignKey(Issue, on_delete=models.CASCADE)
